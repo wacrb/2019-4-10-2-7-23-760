cb3039608b9800e64e9a63664cdcdadabf3995c9
wacrb <674009348@qq.com>
2019-05-28 14:02:13 +0800

    update main,js and main-spec.js

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
-module.exports = function main(inputs) {
+module.exports = function main(distance,parkTime) {
     // write your code here...
-    return "price";
+	var price=0.0;
+	if(distance<=2){
+		price=6+parkTime*0.25;
+	}
+	else if(distance>2&&distance<=8){
+		price=6+parkTime*0.25+(distance-2)*0.8;
+	}
+	else{
+		price=10.8+parkTime*0.25+(distance-8)*1.2;
+	}
+    return Math.round(price);
 };
